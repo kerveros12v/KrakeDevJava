05939c41d6e7379dd9d0ed23b7f00859704c6d77
kerveros12v <kerveros12v@aol.com>
2026-02-24 19:47:40 -0500

    Sentencia else If

--- ControlFlujo/src/com/krakedev/controlflujo/Validador.java
+++ ControlFlujo/src/com/krakedev/controlflujo/Validador.java
@@ -5,8 +5,14 @@ public void validarEdad(int edad) {
 		if(edad>=18) {
 			System.out.println("Es mayor de edad");
 		}
+		else if(edad>=12 && edad<18){
+			System.out.println("Es adolecente");
+		}
+		else if (edad>0 && edad<12){
+			System.out.println("Es niño");
+		}
 		else {
-			System.out.println("Es menor de edad");
+			System.out.println("Edad incorrecta");
 		}
 }
 }
