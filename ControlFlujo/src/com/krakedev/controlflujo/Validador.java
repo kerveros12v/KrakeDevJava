e00e9b03ae7bd898d34cc901bf8b8423509d6dde
kerveros12v <kerveros12v@aol.com>
2026-02-24 19:41:08 -0500

    Sentencia If

---
+++ ControlFlujo/src/com/krakedev/controlflujo/Validador.java
@@ -0,0 +1,12 @@
+package com.krakedev.controlflujo;
+
+public class Validador {
+public void validarEdad(int edad) {
+		if(edad>=18) {
+			System.out.println("Es mayor de edad");
+		}
+		else {
+			System.out.println("Es menor de edad");
+		}
+}
+}
---
+++ ControlFlujo/src/com/krakedev/controlflujo/test/TestValidador.java
@@ -0,0 +1,12 @@
+package com.krakedev.controlflujo.test;
+
+import com.krakedev.controlflujo.Validador;
+
+public class TestValidador {
+public static void main(String[] args) {
+		Validador validador=new Validador();
+		validador.validarEdad(10);
+		validador.validarEdad(20);
+		validador.validarEdad(18);
+}
+}
