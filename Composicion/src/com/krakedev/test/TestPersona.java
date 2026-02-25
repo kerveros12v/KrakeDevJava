577e5c21000087ec21192a44b08479bff64ea30c
kerveros12v <kerveros12v@aol.com>
2026-02-24 22:50:38 -0500

    Valores por defecto

---
+++ Composicion/src/com/krakedev/test/TestPersona.java
@@ -0,0 +1,12 @@
+package com.krakedev.test;
+
+import com.krakedev.composicion.Persona;
+
+public class TestPersona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persona persona1 = new Persona();
+	}
+
+}
