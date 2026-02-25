abfbb2ba420ebaaefd1439611116319e2f358788
kerveros12v <kerveros12v@aol.com>
2026-02-24 23:06:12 -0500

    NullPointerException

--- Composicion/src/com/krakedev/test/TestPersona.java
+++ Composicion/src/com/krakedev/test/TestPersona.java
@@ -1,5 +1,6 @@
 package com.krakedev.test;
 
+import com.krakedev.composicion.Direccion;
 import com.krakedev.composicion.Persona;
 
 public class TestPersona {
@@ -7,6 +8,10 @@ public class TestPersona {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Persona persona1 = new Persona();
+		String nombre=persona1.getNombre();
+		Direccion direccion=persona1.getDireccion();
+		System.out.println(nombre);
+		System.out.println(direccion.getCallePrincipal());
 	}
 
 }
