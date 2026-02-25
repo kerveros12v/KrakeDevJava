8ae745caa43e770028d53aa25a92998063a04884
kerveros12v <kerveros12v@aol.com>
2026-02-24 23:13:18 -0500

    Completar informacion

--- Composicion/src/com/krakedev/test/TestPersona.java
+++ Composicion/src/com/krakedev/test/TestPersona.java
@@ -8,6 +8,14 @@ public class TestPersona {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Persona persona1 = new Persona();
+		persona1.setNombre("Fernando");
+		Direccion dir=new Direccion();
+		dir.setCallePrincipal("Av. Siempre viva");
+		dir.setCalleSecundaria("Falsa");
+		dir.setNumero("123");
+		persona1.setDireccion(dir);
+		
+		
 		String nombre=persona1.getNombre();
 		Direccion direccion=persona1.getDireccion();
 		System.out.println(nombre);
