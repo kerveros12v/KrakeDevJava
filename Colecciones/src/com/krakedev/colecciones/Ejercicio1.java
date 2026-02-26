ef832b34b443b623f7c690389d48f40906a70919
kerveros12v <kerveros12v@aol.com>
2026-02-26 10:44:48 -0500

    Crear Array List

---
+++ Colecciones/src/com/krakedev/colecciones/Ejercicio1.java
@@ -0,0 +1,10 @@
+package com.krakedev.colecciones;
+
+import java.util.ArrayList;
+
+public class Ejercicio1 {
+public static void main(String[] args) {
+	ArrayList<String>cadenas=new ArrayList<String>();
+	System.out.println(cadenas.size());
+}
+}
