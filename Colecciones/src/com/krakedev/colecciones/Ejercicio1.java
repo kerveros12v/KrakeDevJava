31da66a4dde23a5797d142ea187d1a439421b6cb
kerveros12v <kerveros12v@aol.com>
2026-02-26 22:50:08 -0500

    Agregar elemento array

--- Colecciones/src/com/krakedev/colecciones/Ejercicio1.java
+++ Colecciones/src/com/krakedev/colecciones/Ejercicio1.java
@@ -6,5 +6,8 @@ public class Ejercicio1 {
 public static void main(String[] args) {
 	ArrayList<String>cadenas=new ArrayList<String>();
 	System.out.println(cadenas.size());
+	cadenas.add("uno");
+	cadenas.add("dos");
+	System.out.println(cadenas.size());
 }
 }
