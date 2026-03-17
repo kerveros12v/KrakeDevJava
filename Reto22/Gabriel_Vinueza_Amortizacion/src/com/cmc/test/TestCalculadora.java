c6ba05c7393908c18f47780a5a366b0e24a07e45
kerveros12v <kerveros12v@aol.com>
2026-03-16 20:59:40 -0500

    probando con otros valores

--- Reto22/Gabriel_Vinueza_Amortizacion/src/com/cmc/test/TestCalculadora.java
+++ Reto22/Gabriel_Vinueza_Amortizacion/src/com/cmc/test/TestCalculadora.java
@@ -5,7 +5,7 @@ import com.cmc.evaluacion.servicios.CalculadoraAmortizacion;
 
 public class TestCalculadora {
     public static void main(String[] args) {
-        Prestamo prestamo = new Prestamo(5000, 12, 12);
+        Prestamo prestamo = new Prestamo(7000, 24, 36);
 
         CalculadoraAmortizacion.generarTabla(prestamo);
         CalculadoraAmortizacion.mostrarTabla(prestamo);
