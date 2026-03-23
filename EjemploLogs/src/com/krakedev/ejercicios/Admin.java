c3dcc85d0fe1aaf21759ee08f94fb2dff4864001
Vinueza Pozo Gabriel Fernando <kerveros12v@aol.com>
2026-03-23 18:45:05 -0500

    Utilizar Log4j

---
+++ EjemploLogs/src/com/krakedev/ejercicios/Admin.java
@@ -0,0 +1,12 @@
+package com.krakedev.ejercicios;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Admin {
+	private static Logger logger = LogManager.getLogger(Admin.class);
+
+	public void agregar() {
+		logger.info("ingresa a agregar");
+	}
+}
