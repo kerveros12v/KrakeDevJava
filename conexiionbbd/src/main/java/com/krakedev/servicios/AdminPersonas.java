f220c3fd46713044778e67ea8cb2f72196867773
Vinueza Pozo Gabriel Fernando <kerveros12v@aol.com>
2026-04-01 14:50:13 -0500

    Cerrar conexion

--- conexiionbbd/src/main/java/com/krakedev/servicios/AdminPersonas.java
+++ conexiionbbd/src/main/java/com/krakedev/servicios/AdminPersonas.java
@@ -1,14 +1,27 @@
 package com.krakedev.servicios;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import com.krakedev.persistencia.entidades.Persona;
 import com.krakedev.persistencia.utils.ConexionBDD;
 
 public class AdminPersonas {
 public void insertar(Persona persona) {
+	Connection con=null;
+	try {
+	con=ConexionBDD.conectar();
+	} catch (Exception e) {
+		// TODO: handle exception
+	}finally {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
-	
-			Connection con=ConexionBDD.conectar();
+		
 	
 	
 }
